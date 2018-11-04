0428ea523c2188d05b5f59b790092eb0e6944c7d
maksad <d.maksad@gmail.com>
2018-11-04 19:59:35 +0200

    Add code ex7_q2 separate model

--- ex7/ex7_q2.py
+++ ex7/ex7_q2.py
@@ -9,7 +9,7 @@ import pandas as pd
 import pystan
 
 #%% The data
-machines = pd.read_fwf('./ex7/factory.txt').values
+machines = pd.read_fwf('./ex7/factory.txt', header=None).values
 
 #%% Pooled model
 '''
@@ -36,17 +36,66 @@ generated quantities {
 #%% fitting data to stan model
 machines_pooled = machines.flatten()
 model_pooled = pystan.StanModel(model_code=stan_code_pooled)
-data = dict(
+data_pooled = dict(
     N=machines_pooled.size,
     y=machines_pooled
 )
 
 #%% sampling
-fit_pooled = model_pooled.sampling(data=data)
+fit_pooled = model_pooled.sampling(data=data_pooled)
 print(fit_pooled)
 
 #%% hist
 y_pred_pooled = fit_pooled.extract()['ypred']
 plt.hist(y_pred_pooled, bins=20, ec='white')
 plt.savefig('./ex7/report/pooled_hist.png')
+plt.figure(0)
+
+#%% Separate model
+stan_code_separate = '''
+data {
+    int<lower=0> N;               // number of data points
+    int<lower=0> K;               // number of groups
+    int<lower=1,upper=K> x[N];    // group indicator
+    vector[N] y;
+}
+parameters {
+    vector[K] mu;                 // group means
+    vector<lower=0>[K] sigma;     // group stds
+}
+model {
+    y ~ normal(mu[x], sigma[x]);
+}
+generated quantities {
+    real ypred;
+    ypred = normal_rng(mu[6], sigma[6]);
+}
+'''
+
+#%% fitting data into the stan model
+model_seperate = pystan.StanModel(model_code=stan_code_separate)
+machines_transposed = machines.T
+data_separate = dict(
+    N=machines_transposed.size,
+    K=6,
+    x=[
+        1, 1, 1, 1, 1,
+        2, 2, 2, 2, 2,
+        3, 3, 3, 3, 3,
+        4, 4, 4, 4, 4,
+        5, 5, 5, 5, 5,
+        6, 6, 6, 6, 6,
+    ],
+    y=machines_transposed.flatten()
+)
+
+#%% sampling
+fit_separate = model_seperate.sampling(data=data_separate, n_jobs=-1)
+print(fit_separate)
+fit_separate.summary()
+
+#%% hist
+y_pred_separate = fit_separate.extract()['ypred']
+plt.hist(y_pred_separate, bins=20, ec='white')
+plt.savefig('./ex7/report/separate_hist.png')
 plt.figure(0)

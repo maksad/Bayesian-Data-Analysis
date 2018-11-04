bb38707e2012099abc939c5f6b03da613b14209e
maksad <d.maksad@gmail.com>
2018-11-04 17:07:46 +0200

    Add code ex7_q2 pool model

--- ex7/ex7_q2.py
+++ ex7/ex7_q2.py
@@ -8,19 +8,45 @@ import numpy as np
 import pandas as pd
 import pystan
 
-stan = '''
-// Comparison of k groups with common variance
+#%% The data
+machines = pd.read_fwf('./ex7/factory.txt').values
+
+#%% Pooled model
+'''
+Pooled model
+'''
+stan_code_pooled = '''
 data {
-  int<lower=0> N; // number of data points
-  int<lower=0> K; // number of groups
-  int<lower=1,upper=K> x[N]; // group indicator
-  vector[N] y; //
+    int<lower=0> N;       // number of data points
+    vector[N] y;          //
 }
 parameters {
-  vector[K] mu;        // group means
-  real<lower=0> sigma; // common std
+    real mu;              // group means
+    real<lower=0> sigma;  // common std
 }
 model {
-  y ~ normal(mu, sigma);
+    y ~ normal(mu, sigma);
+}
+generated quantities {
+    real ypred;
+    ypred = normal_rng(mu, sigma);
 }
 '''
+
+#%% fitting data to stan model
+machines_pooled = machines.flatten()
+model_pooled = pystan.StanModel(model_code=stan_code_pooled)
+data = dict(
+    N=machines_pooled.size,
+    y=machines_pooled
+)
+
+#%% sampling
+fit_pooled = model_pooled.sampling(data=data)
+print(fit_pooled)
+
+#%% hist
+y_pred_pooled = fit_pooled.extract()['ypred']
+plt.hist(y_pred_pooled, bins=20, ec='white')
+plt.savefig('./ex7/report/pooled_hist.png')
+plt.figure(0)
